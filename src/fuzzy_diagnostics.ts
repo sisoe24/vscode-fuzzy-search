c657454a874d5c03530047df257db35b8e184ad2
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-08-20 10:45:56 -0400

    Cleanup old reference

--- src/fuzzy_diagnostics.ts
+++ src/fuzzy_diagnostics.ts
@@ -25,12 +25,11 @@ export function getFileDiagnostics(fileName: string, level: vscode.DiagnosticSev
     const allDiagnostics = vscode.languages.getDiagnostics();
     const currentFileDiagnostics = allDiagnostics.filter((diag) => diag[0].path === fileName)[0][1];
 
-    const errors = currentFileDiagnostics;
     const items: Item[] = [];
 
-    for (let i = 0; i < errors.length; i++) {
-        const error = errors[i];
-        const errNum = pad((i + 1).toString(), errors.length.toString().length);
+    for (let i = 0; i < currentFileDiagnostics.length; i++) {
+        const error = currentFileDiagnostics[i];
+        const errNum = pad((i + 1).toString(), currentFileDiagnostics.length.toString().length);
         const errObj = diagnosticIcons[error.severity];
 
         const loc = `Ln ${error.range.start.line + 1}, Col ${error.range.start.character + 1}`;
